59744e889c077c375457760d30973d91aa5c9e17
hussainahmaddev <hussainahmaddev@gmail.com>
2024-03-12 12:34:14 +0500

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
+import { Box } from "@mui/material";
 import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-const Dasboard = lazy(() => import("./pages/attendance"));
+import error_404 from "./assets/404-error.gif";
+const Dasboard = lazy(() => import("./pages/dashboard"));
+const Attendance = lazy(() => import("./pages/attendance"));
 const DefaultLayout = lazy(() => import("./layouts/default"));
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="*"
+          path="/"
           element={
             <DefaultLayout>
               <Dasboard />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/attendance"
+          element={
+            <DefaultLayout>
+              <Attendance />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <DefaultLayout>
+              <Box
+                component={"img"}
+                src={error_404}
+                textAlign={"center"}
+                width={"100%"}
+                maxWidth={"500px"}
+                m={"auto"}
+                display={"block"}
+              />
             </DefaultLayout>
           }
         />
